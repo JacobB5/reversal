2ad5ed5de09a86c7d7c031eceb5ef7d37b5fa569
JacobB5 <Jake.Bechtel.5@gmail.com>
2015-10-12 20:03:53 -0400

    Still need to read in but write is done

--- src/Reversal.java
+++ src/Reversal.java
@@ -1,12 +1,49 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Reversal {
-    
+
     public static void reverseFile(File input, File output)
-            throws  FileNotFoundException{
+            throws FileNotFoundException {
+        //Throw exception if either file is missing
+        if ( (!input.exists()) || (!output.exists()) ){
+            throw new FileNotFoundException();
+        }
         
+        ArrayList<String> lines = new ArrayList<String>();
+        String temp = "";
+        String feed = "";
         
-    }
+        //Read in data
+        try {
+            
+            Scanner inp = new Scanner(input);
+            while (inp.hasNext()) {
+                
+
+            }
+
+            // Write stored strings to output
+            PrintWriter out = new PrintWriter(output);
+            for (int i = (lines.size() - 1); i > -1; i--) {
+                out.println(lines.get(i));
+            }
+            // Done so close the output
+            out.close();
+            // Done so close the input
+            inp.close();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+        }
+
+    }   
 
 }
