265bc1cc4299d9faf2d51a1ac58a6a2241213f45
JacobB5 <Jake.Bechtel.5@gmail.com>
2015-10-13 14:44:42 -0400

    Read finished, tests identified

--- src/ReversalTest.java
+++ src/ReversalTest.java
@@ -4,38 +4,46 @@ import org.junit.Test;
 
 public class ReversalTest {
 
-    @Test
+    @Test 
     public void testReversal1() {
+        //Send a non-existant input file
+        
         fail("Not yet implemented");
     }
     
     @Test
     public void testReversal2() {
+      //Send a non-existant output file
         fail("Not yet implemented");
     }
     
     @Test
     public void testReversal3() {
+      //Send an empty input file
         fail("Not yet implemented");
     }
     
     @Test
     public void testReversal4() {
+        //Send a file one line, one word
         fail("Not yet implemented");
     }
     
     @Test
     public void testReversal5() {
+      //Send a file one line, two words
         fail("Not yet implemented");
     }
     
     @Test
     public void testReversal6() {
+      //Send a file two line, two words
         fail("Not yet implemented");
     }
     
     @Test
     public void testReversal7() {
+        //Three lines, middle line blank
         fail("Not yet implemented");
     }
 
