18a77879c9966db855ae87d0ac9397d20657c731
JacobB5 <Jake.Bechtel.5@gmail.com>
2015-10-17 22:49:43 -0400

    Final tests completed

--- src/Reversal.java
+++ src/Reversal.java
@@ -1,60 +1,45 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reversal {
-
     public static void reverseFile(File input, File output)
             throws FileNotFoundException {
-        //Throw exception if either file is missing
-        if ( (!input.exists()) || (!output.exists()) ){
+        // Throw exception if either file is missing
+        if ((!input.exists()) || (!output.exists())) {
             throw new FileNotFoundException();
         }
-        
+
         ArrayList<String> lines = new ArrayList<String>();
         String temp = "";
         String feed = "";
+
+        // Read in data
+        Scanner inp = new Scanner(input);
+        while (inp.hasNext()) {
+            temp = inp.nextLine();
+            String[] tokens = temp.split("\\s+");
+            // Create a string which has the words in reverse order
+            for (int i = tokens.length - 1; i > -1; i--) {
+                feed = feed + tokens[i] + " ";
+            }
+            feed = feed.trim();
+            lines.add(feed);
+            feed = "";
+            temp = "";
+        }
+
+        // Write stored strings to output
+        PrintWriter out = new PrintWriter(output);
+        for (int i = (lines.size() - 1); i > -1; i--) {
+            out.println(lines.get(i));
+        }
         
-        //Read in data
-        try { // 
-            
-            Scanner inp = new Scanner(input);
-            while (inp.hasNext()) {
-                temp = inp.nextLine();
-                String[] tokens = temp.split("\\s+");
-                // Create a string which has the words in reverse order
-                if (tokens.length > 0) {
-                    for (int i = tokens.length - 1; i > -1; i--) {
-                        feed = feed + tokens[i] + " ";
-                    }
-                }
-                feed = feed.trim();
-                lines.add(feed);
-                feed = "";
-                temp = "";                
-
-            }
-
-            // Write stored strings to output
-            PrintWriter out = new PrintWriter(output);
-            for (int i = (lines.size() - 1); i > -1; i--) {
-                out.println(lines.get(i));
-            }
-            // Done so close the output
-            out.close();
-            // Done so close the input
-            inp.close();
-        }
-
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-        }
-
-    }   
+        // Done, so close the input and output
+        out.close();
+        inp.close();
+    }
 
 }
--- src/ReversalTest.java
+++ src/ReversalTest.java
@@ -5,16 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 public class ReversalTest {
-    //public static TemporaryFolder tempFolder;
     public static File inputOne;
     public static File inputTwo;
     public static File inputThree;
@@ -27,108 +23,108 @@ public class ReversalTest {
     public static File outputFive;
     public static File fake;
     public static Scanner reader;;
-    
+
     @ClassRule
     public static TemporaryFolder tempFolder = new TemporaryFolder();
-    
+
     @BeforeClass
-    public static void executeOnce(){
-        fake=new File("this_file_does_not_exist.txt");
-        //Create the input files and output files
+    public static void executeOnce() {
+        fake = new File("this_file_does_not_exist.txt");
+        // Create the input files and output files
         try {
-            //tempFolder = new TemporaryFolder();
-            //empty file
-            inputOne=tempFolder.newFile("inp1.txt");
+
+            // empty file
+            inputOne = tempFolder.newFile("inp1.txt");
             PrintWriter writing = new PrintWriter(inputOne);
             writing.println("");
             writing.close();
-            
-            //One Line, One word
-            inputTwo=tempFolder.newFile("inp2.txt");
+
+            // One Line, One word
+            inputTwo = tempFolder.newFile("inp2.txt");
             writing = new PrintWriter(inputTwo);
             writing.println("Ted");
             writing.close();
-                        
-            //One Line, Two Words
-            inputThree=tempFolder.newFile("inp3.txt");
+
+            // One Line, Two Words
+            inputThree = tempFolder.newFile("inp3.txt");
             writing = new PrintWriter(inputThree);
             writing.println("Ted roses");
             writing.close();
-            
-            //Two lines, two words each
-            inputFour=tempFolder.newFile("inp4.txt");
+
+            // Two lines, two words each
+            inputFour = tempFolder.newFile("inp4.txt");
             writing = new PrintWriter(inputFour);
             writing.println("Ted roses");
             writing.println("much work.");
             writing.close();
-            
-            //Three lines, one empty row
-            inputFive=tempFolder.newFile("inp5.txt");
+
+            // Three lines, one empty row
+            inputFive = tempFolder.newFile("inp5.txt");
             writing = new PrintWriter(inputFive);
             writing.println("Ted roses");
             writing.println("");
             writing.println("much work.");
             writing.close();
-            
-            outputOne=tempFolder.newFile("out1.txt");
+
+            outputOne = tempFolder.newFile("out1.txt");
             writing = new PrintWriter(outputOne);
             writing.println("");
             writing.close();
-            
-            outputTwo=tempFolder.newFile("out2.txt");
-            outputThree=tempFolder.newFile("out3.txt");
-            outputFour=tempFolder.newFile("out4.txt");
-            outputFive=tempFolder.newFile("out5.txt");
+
+            outputTwo = tempFolder.newFile("out2.txt");
+            outputThree = tempFolder.newFile("out3.txt");
+            outputFour = tempFolder.newFile("out4.txt");
+            outputFive = tempFolder.newFile("out5.txt");
         }
         catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }    
+    }
 
-    @Test 
+    @Test
     public void testReversal1() {
-        //Send a non-existent input file
-        int actual=1;
-        int expected=2;
+        // Send a non-existent input file
+        int actual = 1;
+        int expected = 2;
         try {
             Reversal.reverseFile(fake, outputOne);
         }
         catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
-            //e.printStackTrace();
-            actual=2;
+            // e.printStackTrace();
+            actual = 2;
         }
-        assertEquals("Exception not thrown", actual, expected );
+        assertEquals("Exception not thrown", actual, expected);
     }
-    
+
     @Test
     public void testReversal2() {
-      //Send a non-existent output file
-        int actual=1;
-        int expected=2;
+        // Send a non-existent output file
+        int actual = 1;
+        int expected = 2;
         try {
             Reversal.reverseFile(inputOne, fake);
         }
         catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
-            //e.printStackTrace();
-            actual=2;
+            // e.printStackTrace();
+            actual = 2;
         }
-        assertEquals("Exception not thrown", actual, expected );
+        assertEquals("Exception not thrown", actual, expected);
     }
-    
+
     @Test
     public void testReversal3() {
-      //Send an empty input file
+        // Send an empty input file
         try {
             Reversal.reverseFile(inputOne, outputOne);
-            reader=new Scanner(outputOne);
-            String actual="";
-            if (reader.hasNext()){
-                actual=reader.nextLine();
+            reader = new Scanner(outputOne);
+            String actual = "";
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
             }
-            String expected="";
+            String expected = "";
             assertEquals("File was not empty", expected, actual);
             assertFalse("File still has data", reader.hasNext());
         }
@@ -136,46 +132,112 @@ public class ReversalTest {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
     }
-    
+
     @Test
     public void testReversal4() {
-        
+        // One line, One word
         try {
             Reversal.reverseFile(inputTwo, outputTwo);
-            reader=new Scanner(outputOne);
-            String actual="";
-            if (reader.hasNext()){
-                actual=reader.nextLine();
+            reader = new Scanner(outputTwo);
+            String actual = "";
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
             }
-            String expected="Ted";
-            assertEquals("File was not empty", expected, actual);
+            String expected = "Ted";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
             assertFalse("File still has data", reader.hasNext());
         }
         catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-      
+
     }
-    
+
     @Test
     public void testReversal5() {
-      //Send a file one line, two words
-        fail("Not yet implemented");
+        // Send a file one line, two words
+        try {
+            Reversal.reverseFile(inputThree, outputThree);
+            reader = new Scanner(outputThree);
+            String actual = "";
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
+            }
+            String expected = "roses Ted";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
+            assertFalse("File still has data", reader.hasNext());
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
-    
+
     @Test
     public void testReversal6() {
-      //Send a file two line, two words
-        fail("Not yet implemented");
+        // Send a file two line, two words
+        try {
+            Reversal.reverseFile(inputFour, outputFour);
+            reader = new Scanner(outputFour);
+            String actual = "";
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
+            }
+            String expected = "work. much";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
+            }
+            expected = "roses Ted";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
+            assertFalse("File still has data", reader.hasNext());
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-    
+
     @Test
     public void testReversal7() {
-        //Three lines, middle line blank
-        fail("Not yet implemented");
+        // Three lines, middle line blank
+        try {
+            Reversal.reverseFile(inputFive, outputFive);
+            reader = new Scanner(outputFive);
+            String actual = "";
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
+            }
+            String expected = "work. much";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
+            }
+            expected = "";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
+            if (reader.hasNext()) {
+                actual = reader.nextLine();
+            }
+            expected = "roses Ted";
+            assertEquals("Expected:" + expected + " Received:" + actual,
+                    expected, actual);
+            assertFalse("File still has data", reader.hasNext());
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
 }
